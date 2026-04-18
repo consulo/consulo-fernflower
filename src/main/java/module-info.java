2171e8bda81e35d5ced2bba89b2716424b271942
Valery Semenchuk <vistall.valeriy@gmail.com>
2026-04-18 12:56:03 +0300

    drop ide api deps

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,7 +4,16 @@
  */
 module org.jetbrains.fernflower
 {
-	requires consulo.ide.api;
+	requires consulo.execution.api;
+	requires consulo.language.api;
+	requires consulo.language.code.style.api;
+	requires consulo.language.impl;
+	requires consulo.logging.api;
+	requires consulo.project.api;
+	requires consulo.util.io;
+	requires consulo.util.lang;
+	requires consulo.virtual.file.system.api;
+
 	requires asm;
 	requires consulo.java.language.impl;
 	requires fernflower;
